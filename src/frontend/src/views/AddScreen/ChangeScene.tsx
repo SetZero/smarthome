2cf735292a498e98c21b8df567defb379746778d
Felix Trautmann <felix.trautmann@thinking-apps.com>
2021-01-07 15:14:27 +0100

    Update ChangeScene.tsx

--- src/frontend/src/views/AddScreen/ChangeScene.tsx
+++ src/frontend/src/views/AddScreen/ChangeScene.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
@@ -7,13 +7,14 @@ import { addRoom } from '../../reducer/actions/RoomActions'
 import { RoomState, RoomCardSize, ItemRef } from '../../reducer/states/RoomStates'
 import { StateType } from '../../reducer/rootReducer';
 import { SceneState } from '../../reducer/states/SceneStates';
-import { addScene, changeScene } from '../../reducer/actions/SceneActions';
+import { addScene, changeScene, removeItemFromScene } from '../../reducer/actions/SceneActions';
 import { Scenes } from '../Scenes';
 import { AddButton, ElementType, ParentType } from './AddButton';
-import { Grid, Slider, Switch, Typography } from '@material-ui/core';
+import { Container, Grid, IconButton, Paper, Slider, Switch, Typography } from '@material-ui/core';
 import { Item, ItemState } from '../../reducer/states/ItemState';
 import { itemStateChange } from '../../reducer/actions/ItemActions';
 import { ApiService } from '../../Utils/ApiService';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 const useStylesText = makeStyles((theme: Theme) =>
   createStyles({
@@ -50,7 +51,6 @@ export default function ChangeScene({ sceneState, setShowChangeSceneFunction }: 
   const currentScene = scenes.find(e => e.name === sceneState.name);
   /*const [sensors, setSensors] = React.useState<Array<ItemRef>>(sceneState.sensors);*/
 
-
   const handleChangeText = (event: React.ChangeEvent<{ value: unknown }>) => {
     setName(event.target.value as string);
   }
@@ -70,75 +70,93 @@ export default function ChangeScene({ sceneState, setShowChangeSceneFunction }: 
     dispatch(itemStateChange(item));
   }
 
+  async function handleClickOnDeleteItemInScene(e: Item) {
+    var ref: ItemRef = { link: e.link };
+    await dispatch(removeItemFromScene(ref, sceneState.name));
+  }
+
   return (
     <div>
       <div><h1>Szene Ändern</h1></div>
       <div>{sceneState.name}</div>
-
-
-      <form className={classesText.root} noValidate autoComplete="off">
-        <TextField id="standard-basic" label="Name" value={name} defaultValue={sceneState.name} onChange={handleChangeText} />
-      </form>
-      <form className={classesText.root} noValidate autoComplete="off">
-        <TextField id="url" label="Bild Url" value={url} onChange={handleChangeTexturl} />
-      </form>
-
-
-      {items.filter(e => currentScene?.sensors?.find(f => f.link === e.link && e.type == 'Switch') !== undefined).map(e => {
-        return (
-          <Grid className="Left" container alignItems="center" justify="flex-start" item xs spacing={2} key={e.link}>
-            <Grid item sm={8} xs={8}>
-              <Typography variant="h5" component="h6">
-                {e.name}
-              </Typography>
-
+      <Container maxWidth="sm">
+        <Paper variant="outlined" elevation={3}>
+          <Grid container alignItems="center" justify="space-around" spacing={2}>
+            <Grid item sm={10} xs={10}>
+              <form className={classesText.root} noValidate autoComplete="off">
+                <TextField id="standard-basic" label="Name" value={name} defaultValue={sceneState.name} onChange={handleChangeText} />
+              </form>
             </Grid>
-            <Grid item sm={2} xs={2}>
-              <Switch
-                name="unused"
-                inputProps={{ 'aria-label': 'secondary-checkbox' }}
-                onChange={(event, state) => onItemToggle(event, state, e)}
-                checked={e.state === ItemState.ON ?? false}
-              />
+            <Grid item sm={10} xs={10}>
+              <form className={classesText.root} noValidate autoComplete="off">
+                <TextField id="url" label="Bild Url" value={url} onChange={handleChangeTexturl} />
+              </form>
+            </Grid>
+
+            {items.filter(e => currentScene?.sensors?.find(f => f.link === e.link && e.type == 'Switch') !== undefined).map(e => {
+              return (
+                <Grid className="Left" container alignItems="center" justify="flex-start" item xs spacing={2} key={e.link} sm={10}>
+                  <IconButton aria-label="delete" onClick={() => { handleClickOnDeleteItemInScene(e) }}>
+                    <DeleteIcon />
+                  </IconButton >
+                  <Grid item sm={8} xs={8}>
+                    <Typography variant="h5" component="h6">
+                      {e.name}
+                    </Typography>
+
+                  </Grid>
+                  <Grid item sm={2} xs={2}>
+                    <Switch
+                      name="unused"
+                      inputProps={{ 'aria-label': 'secondary-checkbox' }}
+                      onChange={(event, state) => onItemToggle(event, state, e)}
+                      checked={e.state === ItemState.ON ?? false}
+                    />
+                  </Grid>
+                </Grid>
+              )
+            })}
+            {items.filter(e => currentScene?.sensors?.find(f => f.link === e.link && e.type == 'Dimmer') !== undefined).map(e => {
+              return (
+                <Grid className="Left" container alignItems="center" justify="flex-start" item xs spacing={2} sm={10}>
+                  <IconButton aria-label="delete" onClick={() => { handleClickOnDeleteItemInScene(e) }}>
+                    <DeleteIcon />
+                  </IconButton >
+                  <Grid item sm={4} xs={4}>
+                    <Typography variant="h5" component="h6">
+                      {e.name}
+                    </Typography>
+                  </Grid>
+                  <Grid item sm={6} xs={6}>
+                    <Slider defaultValue={20} aria-labelledby="discrete-slider" step={2} marks min={0} max={35}
+                      onChange={(ev, val) => {
+                        ApiService.ChangeDimmer(val + '', e.name);
+                      }}
+
+                    />
+                  </Grid>
+                  <Grid item sm={1} xs={2}>
+                    <Typography>
+                      {e.state}
+                    </Typography>
+                  </Grid>
+                </Grid>
+              )
+            })}
+            <Grid item sm={6} xs={10}>
+              <div className={classesButton.root}>
+                <AddButton type={ElementType.ITEM} parentName={sceneState.name} parentType={ParentType.SCENE} />
+                <Button variant="contained" color="primary" onClick={(e) => {
+                  onChangeScene(sceneState);
+                }}>
+                  Ändern
+                </Button>
+
+              </div>
             </Grid>
           </Grid>
-        )
-      })}
-      {items.filter(e => currentScene?.sensors?.find(f => f.link === e.link && e.type == 'Dimmer') !== undefined).map(e => {
-        return (
-          <Grid className="Left" container alignItems="center" justify="flex-start" item xs spacing={2}>
-            <Grid item sm={3} xs={4}>
-              <Typography variant="h5" component="h6">
-                {e.name}
-              </Typography>
-            </Grid>
-            <Grid item sm={6} xs={6}>
-              <Slider defaultValue={20} aria-labelledby="discrete-slider" step={2} marks min={0} max={35}
-                onChange={(ev, val) => {
-                  ApiService.ChangeDimmer(val + '', e.name);
-                }}
-
-              />
-            </Grid>
-            <Grid item sm={1} xs={2}>
-              <Typography>
-                {e.state}
-              </Typography>
-            </Grid>
-          </Grid>
-        )
-      })}
-      <div className={classesButton.root}>
-        <AddButton type={ElementType.ITEM} parentName={sceneState.name} parentType={ParentType.SCENE} />
-        <Button variant="contained" color="primary" onClick={(e) => {
-          onChangeScene(sceneState);
-        }}>
-          Ändern
-        </Button>
-
-      </div>
-
-
+        </Paper>
+      </Container>
     </div>
   );
 }
