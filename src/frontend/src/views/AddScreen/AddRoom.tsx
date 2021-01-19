ff81e403e20bdf419668a124a3bfff9f2e2ce075
Sebastian <19Sebastian95@gmx.de>
2021-01-20 00:25:29 +0100

    change style

--- src/frontend/src/views/AddScreen/AddRoom.tsx
+++ src/frontend/src/views/AddScreen/AddRoom.tsx
@@ -11,14 +11,15 @@ import { RadioGroup } from '@material-ui/core';
 import { FormLabel } from '@material-ui/core';
 import { FormControlLabel } from '@material-ui/core';
 import { Radio } from '@material-ui/core';
+import { Box } from '@material-ui/core';
 
 
 const useStylesText = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       '& > *': {
-        margin: theme.spacing(1),
-        width: '25ch',
+        padding: theme.spacing(1),
+        width: '100%',
       },
     },
   }),
@@ -29,6 +30,8 @@ const useStylesButton = makeStyles((theme: Theme) =>
       '& > *': {
         margin: theme.spacing(1),
       },
+      justifyContent: "center",
+      display: "flex"
     },
   }),
 );
@@ -36,7 +39,7 @@ interface AddRoomProps {
   closeAnchorFunction: (close: any) => void
 }
 
-export default function AddRoom({closeAnchorFunction}:AddRoomProps) {
+export default function AddRoom({ closeAnchorFunction }: AddRoomProps) {
   const classesText = useStylesText();
   const classesButton = useStylesButton();
   const [name, setName] = React.useState<string>("");
@@ -46,10 +49,10 @@ export default function AddRoom({closeAnchorFunction}:AddRoomProps) {
   const handleChangeOptions = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValue((event.target as HTMLInputElement).value);
   };
-  const handleChangeTexturl = (event: React.ChangeEvent<{value : unknown }>) => {
+  const handleChangeTexturl = (event: React.ChangeEvent<{ value: unknown }>) => {
     setUrl(event.target.value as string);
   }
-  const handleChangeText = (event: React.ChangeEvent<{value : unknown }>) => {
+  const handleChangeText = (event: React.ChangeEvent<{ value: unknown }>) => {
     setName(event.target.value as string);
   }
 
@@ -62,20 +65,22 @@ export default function AddRoom({closeAnchorFunction}:AddRoomProps) {
   }
 
   return (
-    <div>
+    <Box p={2}>
       <Typography variant="h4">
         Raum Hinzufügen
       </Typography>
 
-      <FormControl component="fieldset">
-        <FormLabel component="legend">Darstellungsgröße</FormLabel>
-        <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChangeOptions}>
-          <FormControlLabel value="1" control={<Radio />} label="Klein" />
-          <FormControlLabel value="2" control={<Radio />} label="Mittel" />
-          <FormControlLabel value="3" control={<Radio />} label="Groß" />
-        </RadioGroup>
-      </FormControl>
-      
+      <Box p={2} mt={2}>
+        <FormControl component="fieldset">
+          <FormLabel component="legend">Darstellungsgröße</FormLabel>
+          <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChangeOptions}>
+            <FormControlLabel value="1" control={<Radio />} label="Klein" />
+            <FormControlLabel value="2" control={<Radio />} label="Mittel" />
+            <FormControlLabel value="3" control={<Radio />} label="Groß" />
+          </RadioGroup>
+        </FormControl>
+      </Box>
+
       <form className={classesText.root} noValidate autoComplete="off">
         <TextField id="standard-basic" label="Name" value={name} onChange={handleChangeText} />
       </form>
@@ -84,31 +89,31 @@ export default function AddRoom({closeAnchorFunction}:AddRoomProps) {
         <TextField id="url" label="Bild Url" value={url} onChange={handleChangeTexturl} />
       </form>
 
-      <div className={classesButton.root}>
-        <Button variant="contained" color="primary" onClick= {(e) => { 
-          let pic = url;
-        if (url.length<3){
-          pic=tempPic;
-         }
+        <Box mx="auto" p={1} className={classesButton.root}>
+          <Button variant="contained" color="primary" onClick={(e) => {
+            let pic = url;
+            if (url.length < 3) {
+              pic = tempPic;
+            }
 
-        let room = { name: name, url: pic, cardSize: RoomCardSize.SMALL, sensors: [] };
-        
+            let room = { name: name, url: pic, cardSize: RoomCardSize.SMALL, sensors: [] };
 
-        if (value=="2"){
-          room.cardSize = RoomCardSize.MEDIUM;
-        }
-        else if (value=="3") {
-          room.cardSize = RoomCardSize.LARGE;
-        }
-        onAddRoom(room);
-        setName("");
-        setUrl("")
-      }}>
-      Hinzufügen
-    </Button>
-  </div>
-    
 
-    </div>
+            if (value == "2") {
+              room.cardSize = RoomCardSize.MEDIUM;
+            }
+            else if (value == "3") {
+              room.cardSize = RoomCardSize.LARGE;
+            }
+            onAddRoom(room);
+            setName("");
+            setUrl("")
+          }}>
+            Hinzufügen
+          </Button>
+        </Box>
+
+
+    </Box>
   );
 }
